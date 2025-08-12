07ec3a386dfffc989e7f70c61443e111085bf3d3
lone-wolffie <karenwanjiru09@gmail.com>
2025-08-12 14:44:41 +0300

    Added if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+score = 30
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 70:
+    print("Grade: B") 
+elif score >= 60:
+    print("Grade: C")
+elif score >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
