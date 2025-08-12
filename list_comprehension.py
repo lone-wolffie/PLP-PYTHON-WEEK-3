16c4fc848a1c204cbdf411fa7e4dc0dbb5239260
lone-wolffie <karenwanjiru09@gmail.com>
2025-08-12 15:55:48 +0300

    Added list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,6 @@
+numbers = [x**2 for x in range(5)]
+
+print(numbers)# This code generates a list of squares of numbers from 0 to 4
+
+even_numbers = [x for x in range(10) if x % 2 == 0]
+print(even_numbers) # This code generates a list of even numbers from 0 to 9
